53db05caba5c759615d4d2ae3508dce5d8af982a
Axel <axelpartida584@gmail.com>
2025-06-07 09:18:40 -0600

    Agregando AutoIncrement a entidades

--- chatBotStadistics/src/main/java/com/chatBotStadistics/domain/Respuesta.java
+++ chatBotStadistics/src/main/java/com/chatBotStadistics/domain/Respuesta.java
@@ -1,8 +1,6 @@
 package com.chatBotStadistics.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Respuesta {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "consulta_id", nullable = false)
     private Integer consulta_id;
