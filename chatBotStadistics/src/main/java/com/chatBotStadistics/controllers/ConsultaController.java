6b6c9f3e0ffa40ee7bccee42e339b025ea65b314
Axel <axelpartida584@gmail.com>
2025-06-08 21:53:36 -0600

    Borrando servicios que ya no se necesitan por los ep con parametros opcionales

--- chatBotStadistics/src/main/java/com/chatBotStadistics/controllers/ConsultaController.java
+++ chatBotStadistics/src/main/java/com/chatBotStadistics/controllers/ConsultaController.java
@@ -33,54 +33,6 @@ public class ConsultaController {
     @Autowired
     PromptService promptService;
 
-    @GetMapping("/por-subtema")
-    public Map<String, Double> obtenerEstadisticasPorSubtema() {
-        Map<String, Double> result = consultaService.getEstadisticasPorSubtema();
-        System.out.println("Controller Result: " + result); // Debugging
-        return result;
-    }
-    @GetMapping("/por-tema")
-    public Map<String, Double> obtenerEstadisticasPorTema() {
-        Map<String, Double> result = consultaService.getEstadisticasPorTema();
-        System.out.println("Controller Result: " + result); // Debugging
-        return result;
-    }
-
-
-
-    @GetMapping ("/filtrado")
-    public Long obtenerTotalConsultas(
-            @RequestParam(required = true) Integer year,
-            @RequestParam(required = true) Integer month
-    ) {
-        return consultaService.countConsultas(year, month);
-    }
-    @GetMapping ("/por-year-month-week/filtrado")
-    public Long obtenerEstadisticasPorYearMonthWeek(
-            @RequestParam(required = true) Integer year,
-            @RequestParam(required = true) Integer month,
-            @RequestParam(required = true) Integer week) {
-        return consultaService.countConsultasPorYearMonthWeek(year, month, week);
-    }
-
-    @GetMapping("/por-subtema/filtrado")
-    public Map<String, Double> obtenerEstadisticasPorSubtema(
-            @RequestParam(required = true) Integer year,
-            @RequestParam(required = true) Integer month) {
-        Map<String, Double> result = consultaService.getEstadisticasPorSubtema(year, month);
-        System.out.println("Controller Result: " + result); // Debugging
-        return result;
-    }
-
-    @GetMapping("/por-tema/filtrado")
-    public Map<String, Double> obtenerEstadisticasPorTema(
-            @RequestParam(required = true) Integer year,
-            @RequestParam(required = true) Integer month) {
-        Map<String, Double> result = consultaService.getEstadisticasPorTema(year, month);
-        System.out.println("Controller Result: " + result); // Debugging
-        return result;
-    }
-
     @PostMapping ("/prompt/create")
     public ResponseEntity<Prompt> createPrompt(@Valid @RequestBody PromptRequestDTO promptRequestDTO) {
         Prompt createdPrompt = promptService.createPrompt(promptRequestDTO);
@@ -100,8 +52,6 @@ public class ConsultaController {
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
-
-
 
     //New endpoints
     @GetMapping("/por-temav2")
